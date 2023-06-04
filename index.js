1db52261eca1be84572ca2893d4b8c959f3a744c
tblan21 <tmb2225@gmail.com>
2023-06-04 14:45:02 -0500

    refactored code with express

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express'),
+    morgan = require('morgan'),
+    fs = require('fs'),
+    path = require('path');
+
+const app = express();
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+
+app.use(morgan('combined', {stream: accessLogStream}));
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to myFlix!');
+});
+
+app.use(express.static('public'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+    console.log('My app is listening on port 8080.');
+});
