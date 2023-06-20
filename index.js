2709a6ca9ae3a49c5ae02f90dcef1fb25eef0b1d
tblan21 <tmb2225@gmail.com>
2023-06-20 17:29:11 -0500

    connect to Heroku

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const express = require('express'),
 
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
@@ -236,3 +237,4 @@ const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
   console.log('Listening on Port ' + port);
 });
+
