5b21f7cb696017e32ecf4b95e6e8c52492faacdd
tblan21 <tmb2225@gmail.com>
2023-06-20 17:52:35 -0500

    Revert "update index.js"
    This reverts commit 94d5844b97dfe8dfd07d39c84d67785c7d4d314f.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const express = require('express'),
 const { check, validationResult } = require('express-validator');
 
 // mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb+srv://tblan21:wario52595@cluster1.nzmpjh5.mongodb.net/myFlixDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
